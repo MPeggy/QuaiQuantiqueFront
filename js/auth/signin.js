0fabfe6814bf9ca5dab80afcf79caa7fdaf57837
MPeggy <gypeg426@gmail.com>
2024-11-15 11:38:29 +0100

    Ajout script js+gestion cookie connexion

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -8,7 +8,11 @@ function checkCredentials(){
     //Il faudra appeler l'API ici pour verifier les credentials en BDD
 
     if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){
-        alert("vous etes connectes")
+       
+        //Le vrai token sera à récupérer
+        const token = "lkjsdngfljsqdnglkjsdbglkjqskjgkfjgbqslkfdgbskldfgdfgsdgf";
+        setToken(token);
+        //placer ce token en cookie
 
         window.location.replace("/");
     }
