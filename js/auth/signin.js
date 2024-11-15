b2489304486e31a15a275e0a0b187e143a2e1151
MPeggy <gypeg426@gmail.com>
2024-11-15 11:50:40 +0100

    Ajout bouton déconnexion

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -8,7 +8,6 @@ function checkCredentials(){
     //Il faudra appeler l'API ici pour verifier les credentials en BDD
 
     if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){
-       
         //Le vrai token sera à récupérer
         const token = "lkjsdngfljsqdnglkjsdbglkjqskjgkfjgbqslkfdgbskldfgdfgsdgf";
         setToken(token);
