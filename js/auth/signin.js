ee0960bb241aa196dfc627af69bdc818224f51ce
MPeggy <gypeg426@gmail.com>
2024-11-19 17:33:48 +0100

    Api connexion

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -1,24 +1,47 @@
 const mailInput = document.getElementById("EmailInput");
 const passwordInput = document.getElementById("PasswordInput");
 const btnSignin = document.getElementById("btnSignin");
+const signinForm = document.getElementById("signinForm");
 
 btnSignin.addEventListener("click", checkCredentials);
 
 function checkCredentials(){
-    //Il faudra appeler l'API ici pour verifier les credentials en BDD
+    const dataForm = new FormData(signinForm);
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-    if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){
-        //Le vrai token sera à récupérer
-        const token = "lkjsdngfljsqdnglkjsdbglkjqskjgkfjgbqslkfdgbskldfgdfgsdgf";
+    const raw = JSON.stringify({
+        "username": dataForm.get("Email"),
+        "password": dataForm.get("Password")
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    fetch(`${apiUrl}login`, requestOptions)
+    .then((response) => {
+        if (response.ok){
+            return response.json();
+        } 
+        else{
+            mailInput.classList.add("is-invalid");
+            passwordInput.classList.add("is-invalid");   
+            
+        }
+    })
+    .then(result => {
+        // token récupéré dans documentation
+        const token = result.apiToken;
         setToken(token);
         //placer ce token en cookie
 
-        setCookie(roleCookieName, "admin", 7);
+        setCookie(roleCookieName, result.roles[0], 7);
 
         window.location.replace("/");
-    }
-    else{
-        mailInput.classList.add("is-invalid");
-        passwordInput.classList.add("is-invalid");    
-    }
+    })
+    .catch((error) => console.error(error));
 }
